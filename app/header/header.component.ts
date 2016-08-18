7b1579d87043b93708ab413dd2865f268b180da6
Thomas Kokholm <tk@moch.dk>
2016-08-18 10:03:35 +0200

    $ and materialize

--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ROUTER_DIRECTIVES } from '@angular/router';
+declare var $:any;
 @Component({
     moduleId: module.id,
     selector: 'app-header',
-    templateUrl: 'header.component.html'
+    templateUrl: 'header.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 export class HeaderComponent implements OnInit {
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        
+        $(".button-collapse").sideNav({
+            closeOnClick: true
+        });
+        
+     }
 
 }
